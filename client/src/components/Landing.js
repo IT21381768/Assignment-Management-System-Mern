c7dde05803f6b937671318797e5ea4496c5b8a99
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-07-31 11:46:11 +0530

    c

---
+++ client/src/components/Landing.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='jumbotron mt-5'>
+                    <div className='col-sm-8 mx-auto' align="center">
+                        <h1>Assignment Management System</h1>
+                        <br/>
+                    <div className='col-sm-8 mx-auto' align="center">
+                        <h1>Welcome</h1>
+                    </div>
+
+                    </div>
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default Landing;
