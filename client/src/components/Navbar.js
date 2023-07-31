2ffb46cb848f59269b2ed065e6e9470e5bbbd829
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-07-31 11:08:03 +0530

    c

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,55 +1,149 @@
-import React, { Component } from 'react';
-import {Link, withRouter} from 'react-router-dom';
+// import React, { Component } from 'react';
+// import { Link , withRouter} from 'react-router-dom';
 
-class Navbar extends Component {
+// class Navbar extends Component {
 
-    logout(e) {
-        e.preventDefault();
-        localStorage.removeItem('usertoken');
-        this.props.history.push('/');
-    }
+//     logout(e) {
+//         e.preventDefault();
+//         localStorage.removeItem('usertoken');
+//         this.props.history.push('/');
+//     }
 
-    render() {
-        const loginRegLink = (
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/register" className="nav-link">Register</Link>
-                </li>
-            </ul>
-        )
-        const userLink = (
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <Link to="/profile" className="nav-link">User</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/uploads" className="nav-link">Assignment Submissions</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/assignments" className="nav-link">Assignment List</Link>
-                </li>
-                <li className="nav-item">
-                    <a href="" onClick={this.logout.bind(this)} className="nav-link">Logout</a>
-                </li>
-            </ul>
-        )
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className="navbar-brand">Assignment Submission Portal</Link>
-                <div className="collapse navbar-collapse" id="navbar1">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link">About</Link>
-                        </li>
-                    </ul>
-                    {localStorage.usertoken ? userLink : loginRegLink}
-                </div>
-            </nav>
-        )
-    }
+//     render() {
+//         const loginRegLink = (
+//             <ul className="nav nav-tabs">
+//                 <li className="nav-item">
+//                     <Link to="/login" className="nav-link"><h1>Login</h1></Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link to="/register" className="nav-link"><h1>Register</h1></Link>
+//                 </li>
+//             </ul>
+//         )
+//         const userLink = (
+//             <ul className="nav nav-tabs">
+//                 <li className="nav-item">
+//                     <Link to="/profile" className="nav-link">User</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link to="/uploads" className="nav-link">Assignment Submissions</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link to="/assignments" className="nav-link">Assignment List</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a href="" onClick={this.logout.bind(this)} className="nav-link">Logout</a>
+//                 </li>
+//             </ul>
+//         )
+//         return (
+//             <nav className="navbar navbar-expand-lg navbar-light rounded">
+//                 <button
+//                     className="navbar-toggler"
+//                     type="button"
+//                     data-toggle="collapse"
+//                     data-target="#navbar1"
+//                     aria-controls="navbar1"
+//                     aria-expanded="false"
+//                     aria-label="Toggle navigation"
+//                 >
+//                     <span className="navbar-toggler-icon"></span>
+//                 </button>
+
+//                 <div
+//                     className="collapse navbar-collapse justify-content-md-center" id="navbar1">
+//                     <ul className="nav nav-tabs">
+//                         <li className="nav-item">
+//                             <Link to="/" className="nav-link">
+//                                 <h1>Home</h1>
+//                             </Link>
+//                         </li>
+//                     </ul>
+//                     {localStorage.usertoken ? userLink : loginRegLink}
+//                 </div>
+//             </nav>
+//         )
+//     }
+// }
+
+// export default withRouter(Navbar);
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('usertoken');
+    window.location.href = '/'; // Navigate programmatically without useHistory
+  };
+
+  const loginRegLink = (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/login" className="nav-link">
+          <h1>Login</h1>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/register" className="nav-link">
+          <h1>Register</h1>
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const userLink = (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/profile" className="nav-link">
+          User
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/uploads" className="nav-link">
+          Assignment Submissions
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/assignments" className="nav-link">
+          Assignment List
+        </Link>
+      </li>
+      <li className="nav-item">
+        <a href="/" onClick={logout} className="nav-link">
+          Logout
+        </a>
+      </li>
+    </ul>
+  );
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light rounded">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar1"
+        aria-controls="navbar1"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse justify-content-md-center" id="navbar1">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <h1>Home</h1>
+            </Link>
+          </li>
+        </ul>
+        {localStorage.usertoken ? userLink : loginRegLink}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
